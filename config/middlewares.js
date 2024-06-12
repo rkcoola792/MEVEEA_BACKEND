707534d1483a2f9099b45b5d46189d79d12df952
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-06-12 09:41:21 +0530

    cors erros fixed

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,29 @@
-module.exports = [
-  'strapi::logger',
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors', // Ensure this middleware is enabled
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // Update this to your frontend origin
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
